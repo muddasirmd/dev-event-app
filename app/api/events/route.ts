6ae30667671ed059389bf008872d7d632595d707
Muddasir <muddasirmd2@gmail.com>
2026-01-12 00:06:01 +0500

    File Upload

--- app/api/events/route.ts
+++ app/api/events/route.ts
@@ -1,3 +1,4 @@
+import { v2 as cloudinary } from 'cloudinary';
 import { NextRequest, NextResponse } from "next/server";
 import connectDB from "@/lib/mongodb";
 import Event from "@/database/event.model";
@@ -18,6 +19,26 @@ export async function POST(req: NextRequest) {
             return NextResponse.json({message: 'Invalid JSON data format'}, {status: 400})
         }
 
+        // Image Upload to Cloudinary
+        const file = formaData.get('image') as File;
+
+        if(!file){
+            return NextResponse.json({message: 'Image file is required'}, {status: 400})
+        }
+
+        const arrayBuffer = await file.arrayBuffer();
+        const buffer = Buffer.from(arrayBuffer);
+
+        const uploadResult = await new Promise((resolve, reject) => {
+            cloudinary.uploader.upload_stream({resource_type: 'image', folder: 'dev-events'}, (error, result) => {
+                if (error) return reject(error);
+
+                resolve(result);
+            }).end(buffer);
+        });
+        
+        event.image = (uploadResult as {secure_url: string}).secure_url;
+        
         const createdEvent = await Event.create(event);
 
         return NextResponse.json({message: 'Event created successfully', event: createdEvent}, {status: 201});
